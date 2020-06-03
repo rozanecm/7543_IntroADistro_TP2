041e293f5e64c48cddebf480ef925bc8d98caf49
luciana <lpiazzi@despegar.com>
2020-06-03 01:14:20 -0300

    primera versión de servidor UDP, con upload file sin garantía de entrega

--- udp_client/upload_file.py
+++ udp_client/upload_file.py
@@ -1,4 +1,55 @@
+import os
+import argparse
+import socket
+
+CHUNK_SIZE=200
+
 def upload_file(server_address, src, name):
   # TODO: Implementar UDP upload_file client
   print('UDP: upload_file({}, {}, {})'.format(server_address, src, name))
+
+  #hago esto medio mock para triggerear acciones y probar mi servidor.
+
+  own_address = ("127.0.0.1", 2020)
+
+  f = open(name, "rb")
+  f.seek(0, os.SEEK_END)
+  size = f.tell()
+  f.seek(0, os.SEEK_SET)
+
+  print("Sending {} bytes from {}".format(size, name))
+
+  # Create socket and connect to server
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  sock.bind(own_address)
+
+  sock.sendto(str("upload_file").encode(), server_address)
+
+  signal, addr = sock.recvfrom(CHUNK_SIZE)
+  if signal.decode() != "start":
+    print("There was an error on the server")
+    return exit(1)
+  
+  #Envío primero el tamaño del archivo para leerlo del lado del servidor.
+  #TODO deberíamos agregar un hash al mensaje de tamaño fijo tipo checksum.
+  sock.sendto(str(size).encode(), server_address)
+
+  # Mando todo el archivo.
+  while True:
+    chunk = f.read(CHUNK_SIZE)
+    if not chunk:
+      break
+    sock.sendto(chunk, server_address)
+
+  
+  # El server me dice cuánta data recibió.
+  num_bytes, addr = sock.recvfrom(CHUNK_SIZE)
+
+  print("Server received {} bytes".format(num_bytes.decode()))
+
+  f.close()
+  sock.close()
+
+
+
   pass
--- udp_server/start_server.py
+++ udp_server/start_server.py
@@ -1,4 +1,61 @@
+
+import argparse
+import socket
+import time
+
+CHUNK_SIZE = 200
+
+def get_timestamp():
+  return int(round(time.time()*1000))
+
 def start_server(server_address, storage_dir):
-  # TODO: Implementar UDP server
   print('UDP: start_server({}, {})'.format(server_address, storage_dir))
+
+  # Creación del socket UDP.
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  sock.bind(server_address)
+
+  while True:
+
+    # Recibo un comando de un cliente, puede ser upload o decode.
+    command, addr = sock.recvfrom(CHUNK_SIZE)
+
+    if command.decode() == "upload_file": 
+      upload_file(sock, storage_dir, addr)
+
+    if command.decode() == "download_file": 
+      download_file(sock)
+
+  sock.close()
+
+def upload_file(sock, storage_dir, client_addr):
+
+  # Mando un mensaje al cliente para que empiece con el envío.
+  # TODO Acá debería poner un timeout o algún mecanismo para que no se trabe si el mensaje no viene.
+  sock.sendto(str("start").encode(), client_addr)
+
+  # Recibo el tamaño del archivo.
+  data, addr = sock.recvfrom(CHUNK_SIZE)
+  size = int(data.decode())
+  print("Incoming file with size {} from {}".format(size, addr))
+
+  # Creo un archivo en el "servidor"
+  filename = "{}/file-{}.bin".format(storage_dir, get_timestamp())
+  f = open(filename, "wb")
+  bytes_received = 0
+
+  # empieza la transmisión de los datos del archivo.
+  while bytes_received < size:
+      data, addr = sock.recvfrom(CHUNK_SIZE)
+      bytes_received += len(data)
+      f.write(data)
+  f.close()
+
+  print("Received file {}".format(filename))
+
+  # Para finalizar mando la cantidad de datos que recibí al cliente
+  sock.sendto(str(bytes_received).encode(),addr)
+
+
+def download_file():
   pass
