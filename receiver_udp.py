24e9dca20006d268679ffe4f29b963f3adf88a06
Matias Rozanec <rozanecm@gmail.com>
2020-06-07 11:58:47 -0300

    add sender and receiver classes

---
+++ receiver_udp.py
@@ -0,0 +1,54 @@
+import socket
+import hashlib
+
+class Receiver:
+    def __init__(self, server_address, chunk_size):
+        self.server_address = server_address
+        self.chunk_size = chunk_size
+
+    def receive_message(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(self.server_address)
+
+        chunks = {}     # K: seq_num; V: chunk data
+
+        end_of_transmission = False
+        while(not end_of_transmission):
+            data, address = self.sock.recvfrom(4+self.chunk_size+32)  # 4: seq num; 5: msg size; 32: checksum
+            #data = data.decode()
+            #print("received", data)
+            seq_number = int(data[:4].decode())
+            print("read seq num:", seq_number)
+            checksum = data[4:4+32].decode()
+            msg = data[4+32:]
+            print("seq_number", seq_number, "msg", msg, "checksum", checksum)
+            if(seq_number == 0):
+                end_of_transmission = True
+                break
+            chunks[seq_number] = msg
+            # TODO check integrity
+            if(self.checksum_ok(msg, checksum)):
+                self.sock.sendto(("ACK" + self.num_to_fix_len_string(seq_number)).encode(), address)
+                print("ACK sent")
+            print("so far:",chunks)
+
+        print(chunks)
+        return self.get_chunks_together(chunks)
+
+    def num_to_fix_len_string(self, num):
+        return str(num).zfill(4)
+
+    def get_chunks_together(self, chunks):
+        chunk_ids = list(chunks.keys())
+        with open("./aux/newtest.txt", 'wb') as file:
+            for id in chunk_ids:
+                file.write(chunks.pop(id))
+                # msg += chunks.pop(id)
+        file.close()
+        #return msg
+
+    def checksum_ok(self, msg, checksum):
+        print("comparing", hashlib.md5(msg).hexdigest(), checksum)
+        return hashlib.md5(msg).hexdigest() == checksum
+
+
---
+++ sender_udp.py
@@ -0,0 +1,74 @@
+import socket
+import threading
+import math
+import hashlib
+
+class Sender:
+    def __init__(self, server_address, chunk_size):
+        self.server_address = server_address
+        self.chunk_size = chunk_size
+        self.receiver_confirmed_end_of_transmission = False #cuando se manda msj de fin de transmision, el receiver debe confirmar que lo recibio
+
+    def send_message(self, msg):
+        print("working with msg:", msg)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        num_of_chunks = self.get_num_of_chunks_for_msg(msg, self.chunk_size)
+        chunks = self.msg_to_chunks(msg, self.chunk_size, num_of_chunks)
+        self.acks = list(range(1,num_of_chunks+1))
+
+        send_thread = threading.Thread(target=self.send_file, args=(self.sock,chunks,self.acks,self.server_address))
+        send_thread.start()
+        recv_thread = threading.Thread(target=self.receive_acks, args=(self.sock,self.acks,))
+        recv_thread.start()
+
+        send_thread.join()
+        recv_thread.join()
+
+    def num_to_fix_len_string(self, num):
+        return str(num).zfill(4)
+
+    def send_file(self, sock, chunks, acks, server_address):
+        while acks:
+            print(acks)
+            for i in acks:
+                print("sending", chunks[i])
+                sock.sendto(self.assemble_msg(chunks[i],i), server_address)
+        # when finished sending file, send eof msg
+        while(not self.receiver_confirmed_end_of_transmission):
+            print('sending ack eof')
+            sock.sendto("0000".encode(), server_address)
+
+    def assemble_msg(self, chunk, seq_number):
+        seq = self.num_to_fix_len_string(seq_number) 
+        ha = hashlib.md5(chunk).hexdigest()
+        #ha = hashlib.md5(str(chunk).encode()).hexdigest()
+        result = seq.encode() + ha.encode() + chunk
+        #result = seq + ha + chunk.decode()
+        return result
+        #return result.encode()
+
+    def receive_acks(self, sock, acks):
+        while acks:
+            print("in receiv acks")
+            data, addres = sock.recvfrom(3+4)  #3:"ACK"; 4: fixed size seq. num
+            data = data.decode()
+            try:
+                acks.remove(int(data[3:]))
+            except ValueError:
+                print("try to remove", data[3:], "resulted in error")
+        #    print("removed", data[3:], "from acks")
+
+        data, addres = sock.recvfrom(3+4)  #3:"ACK"; 4: fixed size seq. num
+        print(data.decode())
+        self.receiver_confirmed_end_of_transmission = True
+
+    def get_num_of_chunks_for_msg(self, msg, chunk_size):
+        return math.ceil(len(msg) / chunk_size)
+
+    def msg_to_chunks(self, msg, chunk_size, num_of_chunks):
+        d = {}
+        for i in range(1, num_of_chunks + 1):
+            d[i] = msg[:chunk_size]
+            msg = msg[chunk_size:]
+        return d
+
